5a6ddc5dc5e4a58119a84ec6eb53645765086f25
ArvindRanganathRaghuraman <raghuraman.arv@northeastern.edu>
2024-12-07 14:24:17 -0500

    Logistics Finalized

---
+++ Desktop/Chronica Clones/Chronica/src/chronica/model/VenueManagementCompany.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chronica.model;
+
+/**
+ *
+ * @author arvindranganathraghuraman
+ */
+class VenueManagementCompany {
+    private String companyId;
+    private String companyName;
+
+    public VenueManagementCompany(String companyId, String companyName) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
+    
+
---
+++ Desktop/Chronica Clones/Chronica/src/chronica/model/business/User/User.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chronica.model.business.User;
+
+/**
+ *
+ * @author Gooqe
+ */
+import chronica.model.business.role.Role;
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+    private int count = 0;
+    
+    
+
+    public User(String username, String pwd, String email, Role role) {
+        count++;
+        this.userId = count;
+        this.username = username;
+        this.password = pwd;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return getUsername();
+    }
+
+}
